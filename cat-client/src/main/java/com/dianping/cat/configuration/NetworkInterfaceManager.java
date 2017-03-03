d843c2816d4a9251960d536c9d789e10739fc5cf
gaoxuan <xuangao@outlook.com>
2017-03-03 12:08:53 +0800

    修改获取本地地址取到127.0.0.1的问题
    获取外网地址

--- cat-client/src/main/java/com/dianping/cat/configuration/NetworkInterfaceManager.java
+++ cat-client/src/main/java/com/dianping/cat/configuration/NetworkInterfaceManager.java
@@ -1,18 +1,43 @@
 package com.dianping.cat.configuration;
 
-import org.unidal.helper.Inets;
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public enum NetworkInterfaceManager {
 	INSTANCE;
 
+	private InetAddress address;
+	
+	private AtomicBoolean m_initialized = new AtomicBoolean();
+	
 	private NetworkInterfaceManager() {
 	}
 
 	public String getLocalHostAddress() {
-		return Inets.IP4.getLocalHostAddress();
+		initialize();
+		return address.getHostAddress();
+//		return Inets.IP4.getLocalHostAddress();
 	}
 
 	public String getLocalHostName() {
-		return Inets.IP4.getLocalHostName();
+		initialize();
+		return address.getHostName();
+//		return Inets.IP4.getLocalHostName();
+	}
+	
+	private synchronized void initialize() {
+		if (!m_initialized.get()) {
+			if (address == null) {
+				try {
+					address = Inet4Address.getLocalHost();
+					m_initialized.set(true);
+				} catch (UnknownHostException e) {
+					String message = "No network configured!";
+					throw new IllegalStateException(message);
+				}
+			}
+		}
 	}
 }
